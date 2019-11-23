b31bd75b21449b930f3a0e52b6c31a8688879b0a
caposcar1998 <oscarcontrerascap@gmail.com>
2019-11-22 18:47:08 -0600

    JUego commit

---
+++ Level creator/Assets/Scripts/MakerTile.cs
@@ -0,0 +1,11 @@
+﻿ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakerTile : MonoBehaviour
+{
+    public Sprite sprite;
+    public int id;
+
+}
---
+++ Level creator/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        gameObject.transform.Translate(new Vector3(h,v )*speed*Time.deltaTime);
+        if (Input.GetKeyDown ("space")){
+           transform.Translate(Vector3.up * 100 * Time.deltaTime, Space.World);
+
+        } 
+    }
+
+
+
+
+}
